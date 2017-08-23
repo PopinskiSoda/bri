0f100a2f8da9bc7bde02479e67697710ca621c53
Тарасов Кирилл Иванович <tarasov-ki@ek.apress.ru>
2017-08-23 18:20:05 +0500

    JS initial commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+;(function() {
+  // function Popup() {
+
+  // }
+
+  // function SlideBlock(imageURL) {
+  //   this.imageURL = imageURL;
+  //   this.comment = '';
+  // }
+
+  // var inputPopup, slideBlockPopup;
+
+  // inputPopup = new Popup();
+  // slideBlockPopup = new Popup();
+
+  function parseImageURLs(value) {
+    JSON.parse(str)
+  }
+
+  parseImageURLs();
+}());
